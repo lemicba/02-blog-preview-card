d1884bd7b6b0764c260583ed27c89f7ae5f44f16
Emiliano Ceballos <lemicba@gmail.com>
2025-02-08 16:43:07 -0300

    fix: border radius on image

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -15,13 +15,13 @@ const Card = ({
   description: string;
 }) => {
   return (
-    <div className="bg-white w-[327px] sm:w-[384px] sm:h-[522px] rounded-[20px] flex flex-col p-300 shadow-dark-100 hover:shadow-dark-200 gap-6 group transition-shadow duration-300 hover:cursor-pointer">
-      <div className="relative h-[200px]">
+    <div className="bg-white w-[327px] sm:w-[384px] sm:h-[522px] rounded-[20px] flex flex-col p-300 shadow-dark-100 hover:shadow-dark-200 gap-6 group transition-shadow duration-300 hover:cursor-pointer border border-gray-950">
+      <div className="relative h-[200px] rounded-[10px]">
         <Image
           src={image}
           alt="Illustration article"
           fill
-          className="rounded-[10px]"
+          className="rounded-[10px] object-cover"
         />
       </div>
       <div className="flex flex-col gap-3 text-black">
